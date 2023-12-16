3023068ab885da01b31f54152a247a5ceb0e3934
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-16 17:21:46 +0100

    finish with the resersh

--- src/Components/Pages/Search.jsx
+++ src/Components/Pages/Search.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { CiSearch } from "react-icons/ci";
-import Img from "../../assets/NotFound_Search.png";
+import ImgNotFound from "../../assets/NotFound_Search.png";
+import ImgMedicin from "../../assets/medicin.png";
+import ImgBank from "../../assets/bank.jpg";
+import ImgTaxi from "../../assets/taxi.png";
+
 export default function Search() {
     const [searchTerm, setSearchTerm] = useState("");
     const items = [
-        { key: "medicin", value: "Aspirin" },
-        { key: "bank", value: "Bank of America" },
-        { key: "taxi", value: "Yellow Taxi" },
-        { key: "medicin", value: "Paracetamol" },
-        { key: "bank", value: "Wells Fargo" },
-        // Add more items with different key-value pairs
+        { key: "medicin", value: "Aspirin", image: ImgMedicin },
+        { key: "bank", value: "Bank of America", image: ImgBank },
+        { key: "taxi", value: "Yellow Taxi", image: ImgTaxi },
+        { key: "medicin", value: "Paracetamol", image: ImgMedicin },
+        { key: "bank", value: "Wells Fargo", image: ImgBank },
+        // Add more items with different key-value pairs and images
     ];
 
     const filteredItems = items.filter((item) =>
@@ -26,6 +30,7 @@ export default function Search() {
                 <div className="flex w-[80%] justify-between items-center shadow rounded">
                     <div className="flex-[70%]">
                         <input
+                            id="search_input"
                             type="text"
                             className="w-full focus:outline-none focus:shadow-none"
                             value={searchTerm}
@@ -37,19 +42,24 @@ export default function Search() {
                     </div>
                 </div>
             </div>
-            {filteredItems.length === 0 && (
+            {filteredItems.length === 0 || searchTerm === "" ? (
                 <div className="flex justify-center items-center h-[60vh]">
-                    <img src={Img} alt="" />
+                    <img src={ImgNotFound} alt="Not Found" />
+                </div>
+            ) : (
+                <div className="flex flex-col gap-4 p-4">
+                    {filteredItems.map((item, index) => (
+                        <div key={index} className="bg-gray-200 p-3 rounded flex justify-start items-center gap-5">
+                            <img
+                                src={item.image}
+                                alt={item.key}
+                                className="w-10 h-10 object-cover rounded-full "
+                            />
+                            <p>{item.value}</p>
+                        </div>
+                    ))}
                 </div>
             )}
-            <div className="flex flex-col gap-4 p-4">
-                {filteredItems.map((item, index) => (
-                    <div key={index} className="bg-gray-200 p-3 rounded">
-                        <p className="font-bold">Key: {item.key}</p>
-                        <p>Value: {item.value}</p>
-                    </div>
-                ))}
-            </div>
         </div>
     );
 }
