33a451110d57d14bb493a846c2a74ac610f2bc19
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-17 00:41:55 +0100

    item Component First Touch

--- src/Components/SearchComponents/Item.jsx
+++ src/Components/SearchComponents/Item.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import { items } from "../Pages/Search";
 import { useParams } from "react-router-dom";
-
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { Link } from "react-router-dom";
 export default function Item() {
     const { key } = useParams();
     const selectedItem = items[key];
-    
+
     return (
-        <div>
-            {selectedItem && (
+        <div className=" relative w-[80%] m-auto flex flex-col justify-center pt-[50px] ">
+            <div
+                className="p-2 rounded  cursor-pointer absolute top-5 left-5  "
+                style={{ boxShadow: "0px 0px 5px 0px gray" }}
+            >
+                <Link to={"/"}>
+                    <IoMdArrowRoundBack className="text-2xl text-purple-500" />
+                </Link>
+            </div>
+
+            {selectedItem ? (
                 <div>
-                    <p>Key: {selectedItem.key}</p>
-                    <p>Value: {selectedItem.value}</p>
-                    <img src={selectedItem.image} alt="" />
-                    {/* Add any other data you want to display */}
+                    <img src={selectedItem.image} alt="" className=" w-[180px] m-auto rounded-full" />
+                </div>
+            ) : (
+                <div>
+                    <img src="" alt="Not Found" />
                 </div>
             )}
         </div>
