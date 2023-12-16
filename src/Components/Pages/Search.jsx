19ca3477a8cd40a594db93347628ecb89174c4d4
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-16 15:36:28 +0100

    init the search bart

--- src/Components/Pages/Search.jsx
+++ src/Components/Pages/Search.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from "react";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { CiSearch } from "react-icons/ci";
 
 export default function Search() {
-  return (
-    <div>Search</div>
-  )
+    return (
+        <div>
+            <div className=" flex justify-center gap-5 p-2">
+                <div
+                    className="p-2 rounded
+                 shadow-lg cursor-pointer"
+                >
+                    <IoMdArrowRoundBack />
+                </div>
+                <div className="flex w-[80%] justify-between items-center shadow rounded">
+                    {/* <div className=" flex-[70%]">
+                        <input type="text" className=" w-full" />
+                    </div> */}
+                    <div className="flex-[70%]">
+                        <input
+                            type="text"
+                            className="p-1 w-full focus:outline-none focus:shadow-none"
+                        />
+                    </div>
+                    <CiSearch className="flex-[5%]" />
+                </div>
+            </div>
+        </div>
+    );
 }
