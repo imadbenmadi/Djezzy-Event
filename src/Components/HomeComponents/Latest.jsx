f97b6d8e356c47c9f7803035c7afa24b6c10adaa
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-16 15:22:37 +0100

    Last eddits in  'last' components

--- src/Components/HomeComponents/Latest.jsx
+++ src/Components/HomeComponents/Latest.jsx
@@ -3,16 +3,32 @@ import React from 'react'
 export default function Latest() {
   return (
       <div>
-          <div className=" ml-2 font-bold">derinier Dossier </div>
+          <div className=" ml-2 mt-4 mb-6 font-bold">derinier Dossier </div>
           <div className=" flex flex-col gap-4 ">
-              <div className=" w-[98%] h-[50px] m-auto rounded bg-black"></div>
-              <div className=" w-[98%] h-[50px] m-auto rounded bg-black"></div>
-              <div className=" w-[98%] h-[50px] m-auto rounded bg-black"></div>
-              <div className=" w-[98%] h-[50px] m-auto rounded bg-black"></div>
-              <div className=" w-[98%] h-[50px] m-auto rounded bg-black"></div>
-              <div className=" w-[98%] h-[50px] m-auto rounded bg-black"></div>
-              <div className=" w-[98%] h-[50px] m-auto rounded bg-black"></div>
-              <div className=" w-[98%] h-[50px] m-auto rounded bg-black"></div>
+              <div
+                  className=" w-[98%] h-[60px] m-auto rounded "
+                  style={{ boxShadow: "0px 0px 4px #bdbdbd" }}
+              ></div>
+              <div
+                  className=" w-[98%] h-[60px] m-auto rounded "
+                  style={{ boxShadow: "0px 0px 10px #bdbdbd" }}
+              ></div>{" "}
+              <div
+                  className=" w-[98%] h-[60px] m-auto rounded "
+                  style={{ boxShadow: "0px 0px 10px #bdbdbd" }}
+              ></div>{" "}
+              <div
+                  className=" w-[98%] h-[60px] m-auto rounded "
+                  style={{ boxShadow: "0px 0px 10px #bdbdbd" }}
+              ></div>{" "}
+              <div
+                  className=" w-[98%] h-[60px] m-auto rounded "
+                  style={{ boxShadow: "0px 0px 10px #bdbdbd" }}
+              ></div>{" "}
+              <div
+                  className=" w-[98%] h-[60px] m-auto rounded "
+                  style={{ boxShadow: "0px 0px 10px #bdbdbd" }}
+              ></div>{" "}
           </div>
       </div>
   );
