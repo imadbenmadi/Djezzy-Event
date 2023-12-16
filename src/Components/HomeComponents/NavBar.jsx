53da5cc0e79832ad5494e78846a569828cd8f08f
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-16 14:45:28 +0100

    finish home components

--- src/Components/HomeComponents/NavBar.jsx
+++ src/Components/HomeComponents/NavBar.jsx
@@ -3,8 +3,8 @@ import { CiSearch } from "react-icons/ci";
 
 export default function NavBar() {
     return (
-        <div className=" fixed top-0 flex w-[100%] ">
-            <div>F </div>
+        <div className=" fixed top-0 flex w-[100%] p-2 shadow ">
+            <div className=" mr-2">F </div>
             <div className=" flex flex-col w-[100%]">
                 <div className=" flex  items-center justify-start ">
                     Bienvenue
