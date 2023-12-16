6663ac35c49d87e75e08a2998ccf4bce456bacb1
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-16 23:01:13 +0100

    NavBar For Laptop

--- src/Components/HomeComponents/NavBar.jsx
+++ src/Components/HomeComponents/NavBar.jsx
@@ -4,9 +4,9 @@ import Logo from "../../assets/Logoo.png";
 import { Link } from "react-router-dom";
 export default function NavBar() {
     return (
-        <div className=" bg-white fixed items-center top-0 flex w-[100%] p-2 shadow ">
+        <div className=" bg-white fixed items-center top-0 flex w-[100%] p-2 shadow-xl ">
             <div className=" mr-2">
-                <img src={Logo} alt="" />
+                <img src={Logo} alt="" className=" w-12" />
             </div>
             <div className=" flex flex-col w-[100%] text-center">
                 <div className="  ">
@@ -18,7 +18,7 @@ export default function NavBar() {
                 <div className=" font-semibold">Vos Dossier en un Clic</div>
             </div>
 
-            <div
+            <div style={{boxShadow:"0px 0px 50px 0px gray"}}
                 className="
              shadow-lg cursor-pointer p-2 rounded-full"
             >
