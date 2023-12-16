e1e42c38059f299df6df8cebd5769802620c1f65
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-16 15:06:45 +0100

    fixing the Logo Probleme

--- src/Components/HomeComponents/Latest.jsx
+++ src/Components/HomeComponents/Latest.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Latest() {
   return (
       <div>
-          <div>derinier Dossier </div>
+          <div className=" ml-2 font-bold">derinier Dossier </div>
+          <div className=" flex flex-col">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+          </div>
       </div>
   );
 }
--- src/Components/HomeComponents/NavBar.jsx
+++ src/Components/HomeComponents/NavBar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { CiSearch } from "react-icons/ci";
-
+import Logo from '../../assets/Logoo.png'  
 export default function NavBar() {
     return (
-        <div className=" fixed top-0 flex w-[100%] p-2 shadow ">
-            <div className=" mr-2">F </div>
+        <div className=" fixed items-center top-0 flex w-[100%] p-2 shadow ">
+            <div className=" mr-2">
+                <img src={Logo} alt="" />
+            </div>
             <div className=" flex flex-col w-[100%] text-center">
                 <div className="  ">
                     Bienvenue
@@ -15,7 +17,8 @@ export default function NavBar() {
                 <div className=" font-semibold">Vos Dossier en un Clic</div>
             </div>
 
-            <div>
+            <div className="
+             shadow-lg cursor-pointer p-2 rounded-full">
                 <CiSearch />
             </div>
         </div>
