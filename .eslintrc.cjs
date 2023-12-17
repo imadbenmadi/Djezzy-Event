dc40968df66fefa277f0d097bbe22b1f5d9ac50d
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-17 09:08:44 +0100

    .

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,10 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
+     "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
     "overrides": [
         {
             "env": {
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import Router from "./Router";
 import { RouterProvider } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
